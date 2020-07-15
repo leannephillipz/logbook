bf1d8936457bb982413ce4f26b96ff1e1f81ecff
Leanne Phillips <leannephillips@Leannes-MBP.home>
2020-07-15 16:52:47 +0100

    vue config - public path

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // publicPath: '/projects/logbook/demo/',
-  publicPath: '',
+  publicPath: '', // path is dependent on directory on live server
   assetsDir: 'assets',
   css: {
     sourceMap: true
